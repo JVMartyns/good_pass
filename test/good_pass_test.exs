0916a1cecfbf4e5b0cc0a75e27083d282928bd8b
JVMartyns <jvmartyns2015@gmail.com>
2022-06-02 08:49:04 -0300

    Test: {module: RandomPass,  functions: generate/0, generate/1}

--- test/good_pass_test.exs
+++ test/good_pass_test.exs
@@ -2,46 +2,13 @@ defmodule GoodPassTest do
   use ExUnit.Case
   doctest GoodPass
 
-  @regex ~r/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
-
-  describe "generate/0" do
-    test "lenght of password" do
-      {:ok, password} = GoodPass.generate()
-      assert String.length(password) == 12
+  describe "goog_pass/1" do
+    test "returns true if the password has 8 characters containing uppercase, lowercase, numbers and symbols." do
+      assert GoodPass.good_password?("G0odP4$5word") == true
     end
 
-    test "contains lowercase, uppercase, numbers and symbols" do
-      {:ok, password} = GoodPass.generate()
-
-      assert String.match?(password, @regex)
-    end
-  end
-
-  describe "generate/1" do
-    test "password with 4 characters" do
-      assert {:error, _message} = GoodPass.generate(4)
-    end
-
-    test "password with 6 characters" do
-      assert {:error, _message} = GoodPass.generate(6)
-    end
-
-    test "password with 8 characters" do
-      assert {:ok, password} = GoodPass.generate(8)
-      assert String.length(password) == 8
-      assert String.match?(password, @regex)
-    end
-
-    test "password with 64 characters" do
-      assert {:ok, password} = GoodPass.generate(64)
-      assert String.length(password) == 64
-      assert String.match?(password, @regex)
-    end
-
-    test "password with 128 characters" do
-      assert {:ok, password} = GoodPass.generate(128)
-      assert String.length(password) == 128
-      assert String.match?(password, @regex)
+    test "returns false if the password not has 8 characters containing uppercase, lowercase, numbers and symbols." do
+      assert GoodPass.good_password?("BadPassword") == false
     end
   end
 end
